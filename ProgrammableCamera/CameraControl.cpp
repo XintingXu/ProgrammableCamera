3f89cf645cfccbd0e71b18cf435f96d9fab898d4
XintingXu <853625143@bit.edu.cn>
2017-02-22 10:31:24 +0800

    Add implementuion of CameraControl.h

---
+++ ProgrammableCamera/CameraControl.cpp
@@ -0,0 +1,55 @@
+#include "CameraControl.h"
+
+/*
+
+
+*/
+
+
+CameraControl::CameraControl(int CameraNumber){
+    ;
+}
+
+CameraControl::~CameraControl(){
+    ;
+}
+
+bool CameraControl::setCamera(int CameraNumber){
+    return true;
+}
+
+bool CameraControl::setBrightness(int Brightness){
+    return true;
+}
+
+bool CameraControl::setContrast(int Contrast){
+    return true;
+}
+
+bool CameraControl::setSaturation(int Saturation){
+    return true;
+}
+
+bool CameraControl::setTone(int Tone){
+    return true;
+}
+
+bool CameraControl::setResolution(int Resolution){
+    return true;
+}
+
+bool CameraControl::setGamma(double Gamma){
+    return true;
+}
+
+bool CameraControl::setWhiteBalance(int WhiteBalance){
+    return true;
+}
+
+bool CameraControl::setExposure(int Exposure){
+    return true;
+}
+
+bool CameraControl::setHightAndWidth(cv::Size2i Size){
+    return true;
+}
